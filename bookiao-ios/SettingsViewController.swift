d00ce7d2aa3418916d953d950b969f679f2e3b2b
Xiomara7 <figueroaxiomara7@gmail.com>
2014-09-30 01:36:26 -0400

    Added tab bar

---
+++ bookiao-ios/SettingsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SettingsViewController.swift
+//  bookiao-ios
+//
+//  Created by Xiomara on 9/30/14.
+//  Copyright (c) 2014 UPRRP. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!){
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
